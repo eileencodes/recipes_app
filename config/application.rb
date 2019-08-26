14ebeb3a377811e8d0001980b81429b45dafe4c0
eileencodes <eileencodes@gmail.com>
2019-08-26 15:32:17 -0400

    Activate db middleware

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,14 @@ Bundler.require(*Rails.groups)
 module RecipesApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.active_record.database_selector = { delay: 2.seconds }
+    config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
+    config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   end
 end
